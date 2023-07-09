c97c7db69abcc86700834643e619f126a1910ca0
11x1 <89593923+11x1@users.noreply.github.com>
2023-07-10 00:54:37 +0300

    Update mr. rob.lua
    eye movement
    minion

--- Primordial.dev/mr.rob, a supportive rectangle extension/mr. rob.lua
+++ Primordial.dev/mr.rob, a supportive rectangle extension/mr. rob.lua
@@ -1,11 +1,32 @@
+local httpFactory = require("primordial/Lightweight HTTP Library.46")
+
+local http = httpFactory.new({
+    task_interval = 0.3, -- polling intervals
+    enable_debug = false, -- print http requests to the console
+    timeout = 10 -- request expiration time
+})
+
+local master_switch = menu.add_checkbox('advanced mr. rob', 'master switch')
+
 local menu_elems = {
-    master_switch = menu.add_checkbox('advanced mr. rob', 'master switch'),
-    style = menu.add_selection('advanced mr. rob', 'what style we picking today sir', {'default', 'elegant', 'neymar'}),
-    skin_color = menu.add_selection('advanced mr. rob', 'race', {'primordial', 'white', 'black'}),
+    style = menu.add_selection('advanced mr. rob', 'what style we picking today sir', { 'default', 'elegant', 'neymar', 'minion' }),
+    skin_color = menu.add_selection('advanced mr. rob', 'race', { 'primordial', 'white', 'black' }),
+    minion_style = menu.add_selection('advanced mr. rob', 'minion style', { 'Bob', 'Carl' }),
 }
+
+render.multiple_lines = function( points, color )
+    for i = 1, #points - 1 do
+        local p1 = points[ i ]
+        local p2 = points[ i + 1 ]
+
+        render.line( p1, p2, color )
+    end
+end
 
 local vars = {
     font = render.create_font('Arial Bold', 16, 400, e_font_flags.ANTIALIAS),
+
+    minions_suit = nil,
 
     expression_offsets = {
         eyebrows = {
@@ -40,6 +61,18 @@ local vars = {
             'Good job!',
             'you > him',
             'You showed that dog its\' place :o'
+        },
+        minion = {
+            'C’est banana! Hahaha! Miam Miam! Huh?',
+            'No, no, no! Hey, a piñata!',
+            'Banana!',
+            'Uh, la cucaracha?',
+            'Luk at u! Kan pai!',
+            'Bee do! Bee do! Bee do! Bee do! Bee do!',
+            'Underwear me want banana!',
+            'Bee doo bee do la la la (farts)',
+            'Ka ko wuddub mi jighum', -- okay some of these are made with translators and are shit because i cant think of any minion sayings oops
+            'BANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANABANANA'
         }
     },
 
@@ -72,21 +105,39 @@ local vars = {
     },
 }
 
+http:get( 'https://shibe.host/LzyDnNjma3v0/direct', function( data )
+    print( data.body )
+    vars.minions_suit = render.load_image_buffer( data.body )
+end )
+
+local menu_handler = function( )
+    for key, _ in pairs( menu_elems ) do
+        menu_elems[ key ]:set_visible( master_switch:get( ) )
+    end
+
+    if not master_switch:get( ) then return end
+    
+    menu_elems.skin_color:set_visible( menu_elems.style:get( ) ~= 4 )
+    menu_elems.minion_style:set_visible( menu_elems.style:get( ) == 4 )
+end
+
 local event_handler = function(event) 
     if event.name == 'player_death' then 
         if engine.get_player_index_from_user_id(event.attacker) == engine.get_local_player_index() then 
             vars.last_kill_time = client.get_unix_time()
-            vars.current_expression = vars.expressions[math.random(1, #vars.expressions)]
+            vars.current_expression = vars.expressions[client.random_int(1, #vars.expressions)]
             local sayings_table
             if menu_elems.style:get() == 2 then
                 sayings_table = vars.sayings.elegant
+            elseif menu_elems.style:get() == 4 then
+                sayings_table = vars.sayings.minion
             else
                 sayings_table = vars.sayings.neymar
             end
     
-            local say = sayings_table[math.random(1, #sayings_table)]
+            local say = sayings_table[client.random_int(1, #sayings_table)]
             if say == vars.saying then
-                say = sayings_table[math.random(1, #sayings_table)]
+                say = sayings_table[client.random_int(1, #sayings_table)]
             end
             vars.saying = say
         end
@@ -117,9 +168,20 @@ local expression_handler = function()
     end
 end
 
-local function on_supportive_rectangle(pos, size, mood, phrase)
-    if not menu_elems.master_switch:get() then return false end
-    
+local get_mouse_angle = function ( origin )
+    local mouse_pos = input.get_mouse_pos( )
+
+    local x = mouse_pos.x - origin.x
+    local y = mouse_pos.y - origin.y
+
+    return math.atan2( y, x )
+end
+
+local angle_to_vector = function( angle )
+    return vec2_t( math.cos( angle ), math.sin( angle ) )
+end
+
+local draw_normal = function ( pos, size, mood, phrase, eye_offset)
     local color = menu_elems.skin_color:get() == 1 and color_t(31, 31, 31, 255) or menu_elems.skin_color:get() == 2 and color_t(232, 190, 172, 255) or menu_elems.skin_color:get() == 3 and color_t(134, 90, 72, 255)
 
     -- render race
@@ -129,19 +191,18 @@ local function on_supportive_rectangle(pos, size, mood, phrase)
     local eyebrow_center = eye_center - vec2_t(0, 17)
     local mouth_center = pos + vec2_t(size.x/2, size.y/3 * 2)
 
-    -- render face & expressions with offsets
     -- eyes
     -- eye bg
     render.circle_filled(eye_center - vec2_t(20, 0), 10, color_t(255, 255, 255, 255))
     render.circle_filled(eye_center + vec2_t(20, 0), 10, color_t(255, 255, 255, 255))
 
     -- pupil (? the colored part i dont englisk)
-    render.circle_filled(eye_center - vec2_t(20, 0), 7, color_t(10, 80, 111, 255))
-    render.circle_filled(eye_center + vec2_t(20, 0), 7, color_t(10, 80, 111, 255))
+    render.circle_filled(eye_center - vec2_t(20, 0) + eye_offset, 7, color_t(10, 80, 111, 255))
+    render.circle_filled(eye_center + vec2_t(20, 0)+ eye_offset, 7, color_t(10, 80, 111, 255))
 
     -- black dot in middle
-    render.circle_filled(eye_center - vec2_t(20, 0), 4, color_t(0, 0, 0, 255))
-    render.circle_filled(eye_center + vec2_t(20, 0), 4, color_t(0, 0, 0, 255))
+    render.circle_filled(eye_center - vec2_t(20, 0) + eye_offset, 4, color_t(0, 0, 0, 255))
+    render.circle_filled(eye_center + vec2_t(20, 0) + eye_offset, 4, color_t(0, 0, 0, 255))
 
     -- moods (eyebrows, eyelid modulating, mouth) make better later
     -- eyebrows
@@ -375,6 +436,119 @@ local function on_supportive_rectangle(pos, size, mood, phrase)
             iteration = iteration + 1
         end
     end
+end
+
+local draw_minion = function( pos, size, mood, phrase, eye_offset )
+    local color = color_t( 245, 204, 76, 255 ) 
+
+    local is_one_eyed = menu_elems.minion_style:get( ) == 2
+
+    local padding = vec2_t( 5, 5 )
+
+    render.push_clip( pos - padding, size + padding + padding )
+
+    -- render race
+    render.rect_filled(pos - padding, size + padding + padding, color, 15)
+
+    local eye_center = pos + vec2_t(size.x/2, size.y/3)
+
+    if is_one_eyed then
+        eye_center = eye_center - vec2_t( 20, 0 )
+    end
+    local eyebrow_center = eye_center - vec2_t(0, 17)
+    local mouth_center = pos + vec2_t(size.x/2, size.y/5 * 3)
+
+    -- render face & expressions with offsets
+    local offset = vars.expression_offsets.eyebrows.outer * 2
+
+    local eyebrow_poly_upper_arc = { }
+    eyebrow_poly_upper_arc[ '1' ] = { }
+    eyebrow_poly_upper_arc[ '-1' ] = { }
+
+    local eyebrow_poly_cleanup = { }
+    eyebrow_poly_cleanup[ '1' ] = { }
+    eyebrow_poly_cleanup[ '-1' ] = { }
+
+
+    eyebrow_poly_upper_arc[ '-1' ][ #eyebrow_poly_upper_arc[ '-1' ] + 1] = eye_center + vec2_t( 4, 0 ) + vec2_t( 16, -7 - offset )
+    eyebrow_poly_upper_arc[ '1' ][ #eyebrow_poly_upper_arc[ '1' ] + 1 ]  = eye_center + vec2_t( -4, 0 ) + vec2_t( -16, -7 - offset )
+    eyebrow_poly_upper_arc[ '-1' ][ #eyebrow_poly_upper_arc[ '-1' ] + 1] = eye_center + vec2_t( 4, 0 ) + vec2_t( 11, -7 - offset/2 )
+    eyebrow_poly_upper_arc[ '1' ][ #eyebrow_poly_upper_arc[ '1' ] + 1 ]  = eye_center + vec2_t( -4, 0 ) + vec2_t( -11, -7 - offset/2 )
+    eyebrow_poly_upper_arc[ '-1' ][ #eyebrow_poly_upper_arc[ '-1' ] + 1] = eye_center + vec2_t( 4, 0 ) + vec2_t( 6, -7 - offset/4 )
+    eyebrow_poly_upper_arc[ '1' ][ #eyebrow_poly_upper_arc[ '1' ] + 1 ]  = eye_center + vec2_t( -4, 0 ) + vec2_t( -6, -7 - offset/4 )
+
+    for i = 10, 170, 10 do
+        local y = -math.sin( math.rad( i ) ) * 10 - 5
+        local progress = i / 180
+        local x = progress * 32
+
+        if progress < 0.5 then
+            x = x + 1.5
+        else
+            x = x - 1.5
+        end
+
+
+        eyebrow_poly_upper_arc[ '-1' ][ #eyebrow_poly_upper_arc[ '-1' ] + 1] = eye_center + vec2_t( 4, 0 ) + vec2_t( x, y )
+        eyebrow_poly_upper_arc[ '1' ][ #eyebrow_poly_upper_arc[ '1' ] + 1 ]  = eye_center + vec2_t( -4, 0 ) + vec2_t( -x, y )
+    end
+
+    render.rect_filled( pos + vec2_t( -padding.x, ( eye_center.y - pos.y ) - 3), vec2_t( size.x + padding.x + padding.x, 6 ), color_t(10, 10, 10, 255), 0 )
+
+    for j = 1, 2 do
+        local i = -j
+        if i % 2 == 0 then i = 1 if is_one_eyed then break end end
+
+        render.circle_filled(eye_center - vec2_t(20 * i, 0), 16, color_t(155, 155, 155, 255))
+
+        -- eye bg
+        render.circle_filled(eye_center - vec2_t(20 * i, 0), 13, color_t(255, 255, 255, 255))
+
+        -- pupil
+        render.circle_filled(eye_center - vec2_t(20 * i, 0) + eye_offset, 6, color_t(10, 80, 111, 255))
+
+        -- black dot in middle
+        render.circle_filled(eye_center - vec2_t(20 * i, 0) + eye_offset, 3, color_t(0, 0, 0, 255))
+
+        render.polygon(  
+            eyebrow_poly_upper_arc[ tostring( i ) ], color
+        )
+
+        render.polygon(  
+            eyebrow_poly_cleanup[ tostring( i ) ], color_t(155, 155, 155, 255)
+        )
+
+
+    end
+
+    -- draw suit
+    if vars.minions_suit ~= nil then
+        render.texture( vars.minions_suit.id, pos + vec2_t( 0, size.y - 40 ) - padding, vec2_t( size.x + 1, 60 ) + padding + padding )
+    end
+
+    render.multiple_lines( {
+        mouth_center + vec2_t( -10, -offset/2 ),
+        mouth_center + vec2_t( -6, -offset/5 ),
+        mouth_center + vec2_t( 0, 0 ),
+        mouth_center + vec2_t( 6, -offset/5 ),
+        mouth_center + vec2_t( 10, -offset/2 ),
+    }, color_t( 0, 0, 0, 255 ))
+
+    render.pop_clip( )
+end
+
+local on_supportive_rectangle = function(pos, size, mood, phrase)
+    if not master_switch:get() then return false end
+
+    local eye_center = pos + vec2_t(size.x/2, size.y/3)
+    local eye_offset = angle_to_vector( get_mouse_angle( eye_center ) ) 
+    eye_offset = eye_offset + eye_offset
+
+    if menu_elems.style:get( ) ~= 4 then
+        draw_normal( pos, size, mood, phrase, eye_offset )
+    else
+        draw_minion( pos, size, mood, phrase, eye_offset )
+    end
 
     -- saying 
     if vars.last_kill_time + 4 > client.get_unix_time() then
@@ -392,10 +566,14 @@ local function on_supportive_rectangle(pos, size, mood, phrase)
             pos + vec2_t(20 + size.x, -15 + text_size.y),
         }, color_t(41, 41, 41, 255))
     end
-
+    
     return true
 end
 
-callbacks.add(e_callbacks.SUPPORTIVE_RECTANGLE, on_supportive_rectangle)
-callbacks.add(e_callbacks.PAINT, expression_handler)
-callbacks.add(e_callbacks.EVENT, event_handler)
+callbacks.add( e_callbacks.SUPPORTIVE_RECTANGLE, on_supportive_rectangle )
+
+callbacks.add( e_callbacks.PAINT, function( )
+    expression_handler( )
+    menu_handler( )
+end )
+callbacks.add( e_callbacks.EVENT, event_handler )
